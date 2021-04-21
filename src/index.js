5f51a459d07eccf13fb356b999da02c70a8df9a4
Ruzanna <ruzanna.tco@gmail.com>
2021-04-21 17:25:33 +0400

    Initial setup webpack, babel, added config file

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { SomeModule } from "./custom";
+
+const instance = new SomeModule();
+instance.test();
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
